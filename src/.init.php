9053d515d761d6dee2347ee3154131f817136e2d
moyo <git@moyo.im>
2018-09-05 17:05:01 +0800

    add tracing layer injector

--- src/.init.php
+++ src/.init.php
@@ -4,4 +4,5 @@ define('CARNO_COMPONENTS_HRPCC', [
     \Carno\HRPC\Client\Components\Invoker::class,
     \Carno\HRPC\Client\Components\Context::class,
     \Carno\HRPC\Client\Components\Routes::class,
+    \Carno\HRPC\Client\Components\Tracing::class,
 ]);
---
+++ src/Components/Tracing.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Tracing injector
+ * User: moyo
+ * Date: 2018/9/5
+ * Time: 5:00 PM
+ */
+
+namespace Carno\HRPC\Client\Components;
+
+use Carno\Console\Component;
+use Carno\Console\Contracts\Application;
+use Carno\Console\Contracts\Bootable;
+use Carno\Container\DI;
+use Carno\HRPC\Client\Handlers\TracedRequesting;
+use Carno\HRPC\Client\Selector;
+use Carno\RPC\Client;
+use Carno\Traced\Contracts\Observer;
+
+class Tracing extends Component implements Bootable
+{
+    /**
+     * @var int
+     */
+    protected $priority = 51;
+
+    /**
+     * @var array
+     */
+    protected $dependencies = [Observer::class];
+
+    /**
+     * @param Application $app
+     */
+    public function starting(Application $app) : void
+    {
+        /**
+         * @var Observer $observer
+         */
+
+        $observer = DI::get(Observer::class);
+
+        $observer->transportable(static function () {
+            Client::layers()->has(TracedRequesting::class)
+                || Client::layers()->append(Selector::class, DI::object(TracedRequesting::class));
+        }, static function () {
+            Client::layers()->remove(TracedRequesting::class);
+        });
+    }
+}
